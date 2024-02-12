f0cabf0ce06494718f8fb714eb2dd2cfd48342c6
Rakhafian <rakhafian9010@gmail.com>
2024-02-13 00:10:52 +0700

    add routes login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\{
     SettingController,
     SupplierController,
     UserController,
+    LoginController,
 };
 use Illuminate\Support\Facades\Route;
 /*
@@ -28,6 +29,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->route('login');
 });
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
