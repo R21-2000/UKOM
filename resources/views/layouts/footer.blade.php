c5fcf3fe0e1510449a8ab01addcfd32c5e252b6a
Zilfs <rheinlandcast@gmail.com>
2024-02-13 00:57:46 +0700

    refactor: kernel

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>by</b> R21
+    </div>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="/">{{ $setting->nama_perusahaan }}</a>.</strong>
+</footer>
